a7aa349088a53b25b818244065117a97bbc98ca4
Sebastian Mira Pacheco <sebastian.mira@ug.uchile.cl>
2024-09-02 12:38:44 -0400

    Half edge structure

---
+++ include/common.h
@@ -0,0 +1,58 @@
+
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <tuple>
+#include <utility>
+#include <list>
+#include <vector>
+#include <limits>
+#include <cstddef>
+#include <cmath>
+
+namespace HalfMesh {
+    // Forward declaration of all classes
+    class Vertex;
+
+    class Face;
+
+    class Edge;
+
+    class HalfEdge;
+
+    class Mesh;
+
+    // Some standard null pointers
+    std::nullptr_t NULL_VERTEX;
+    std::nullptr_t NULL_FACE;
+    std::nullptr_t NULL_EDGE;
+    std::nullptr_t NULL_HALF_EDGE;
+    std::nullptr_t NULL_POINTER;
+
+    // Enum's for Mesh IO
+    enum MESH_TYPE {
+        GMSH = 100,
+        OBJ = 200,
+        BINARY_MESH = 300,
+        VTK = 500,
+        UNKNOWN = 500
+    };
+
+    // Enum for mesh identities
+    enum MESH_ENTITY_TYPE {
+        M_VERTEX,
+        M_EDGE,
+        M_FACE,
+        M_HALF_EDGE
+    };
+
+    // Property addition and deletion status
+    enum PROPERTY_STATUS {
+        PROPERTY_ADDED,
+        PROPERTY_EXISTS,
+        PROPERTY_DELETED,
+        PROPERTY_COULD_NOT_BE_DELETED,
+        PROPERTY_DOES_NOT_EXIST,
+        PROPERTY_COULD_NOT_BE_ADDED
+    };
+}
---
+++ include/half_edge.h
@@ -0,0 +1,89 @@
+#ifndef _HALF_EDGE_
+#define _HALF_EDGE_
+
+#include <general.h>
+#include <vertex.h>
+#include <edge.h>
+#include <face.h>
+
+namespace HalfMesh {
+    // A Half edge structure
+    class HalfEdge {
+    public:
+        HalfEdge(Vertex *_v1, Vertex *_v2) : v1(_v1), v2(_v2) {};
+
+        ~HalfEdge() {};
+
+    public:
+        void set_vertex_one(Vertex *_v1) {
+            v1 = _v1;
+        }
+
+        void set_vertex_two(Vertex *_v2) {
+            v2 = _v2;
+        }
+
+        Vertex *get_vertex_one() {
+            return v1;
+        }
+
+        Vertex *get_vertex_two() {
+            return v2;
+        }
+
+        void set_handle(unsigned int _i) {
+            half_edge_handle = _i;
+        }
+
+        unsigned int handle() {
+            if (this == NULL_HALF_EDGE) {
+                return std::numeric_limits<unsigned int>::max();
+            }
+            return half_edge_handle;
+        }
+
+        void set_parent_edge(unsigned int _i) {
+            parent_edge_handle = _i;
+        }
+
+        unsigned int get_parent_edge() {
+            return parent_edge_handle;
+        }
+
+        void set_parent_face(unsigned int _i) {
+            parent_face_handle = _i;
+        }
+
+        unsigned int get_parent_face() {
+            return parent_face_handle;
+        }
+
+        void set_boundary(bool _boundariness) {
+            boundary_half_edge = _boundariness;
+        }
+
+        bool is_boundary() {
+            return boundary_half_edge;
+        }
+
+        void set_opposing_half_edge(unsigned int _i) {
+            opposing_half_edge = _i;
+        }
+
+        unsigned int get_opposing_half_edge() {
+            return opposing_half_edge;
+        }
+
+
+    private:
+        Vertex *v1, *v2;
+        unsigned int half_edge_handle;
+        unsigned int parent_edge_handle;
+        unsigned int parent_face_handle;
+        unsigned int opposing_half_edge = std::numeric_limits<unsigned int>::max();
+        bool boundary_half_edge;
+    };
+}
+
+
+#endif /* _HALF_EDGE_ */
